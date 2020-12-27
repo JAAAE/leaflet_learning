9e3238d06129fb4391c05efd69c253553f85141d
a354794@gmail.com <a354794@gmail.com>
2020-12-27 14:34:11 +0800

    add

--- docs/js/app.js
+++ docs/js/app.js
@@ -70,17 +70,10 @@ L.control.scale({
 }).addTo(map);
 
 // add Geocoder
-var geocoder = L.Control.geocoder({
-    defaultMarkGeocode: false
-  })
-    .on('markgeocode', function(e) {
-      var bbox = e.geocode.bbox;
-      var poly = L.polygon([
-        bbox.getSouthEast(),
-        bbox.getNorthEast(),
-        bbox.getNorthWest(),
-        bbox.getSouthWest()
-      ]).addTo(map);
-      map.fitBounds(poly.getBounds());
-    })
-    .addTo(map);
+L.Control.geocoder({
+    position: 'topleft',
+    collapsed: false,
+    placeholder: 'Search...',
+    defaultMarkGeocode: true
+    
+      }).addTo(map)   
