671ed93f74996baace18bdf736be7713540e2088
a354794@gmail.com <a354794@gmail.com>
2020-12-28 23:30:40 +0800

    add

--- docs/js/app.js
+++ docs/js/app.js
@@ -109,3 +109,26 @@ axios.get("https://www.geologycloud.tw/api/v1/zh-tw/Fault50?t=.json")
         layer.bindPopup(HTML);
     }
       
+//  measurement control  button
+var plugin = L.control.measure({
+    //  control position
+    position: 'topleft',
+    //  weather to use keyboard control for this plugin
+    keyboard: true,
+    //  shortcut to activate measure
+    activeKeyCode: 'M'.charCodeAt(0),
+    //  shortcut to cancel measure, defaults to 'Esc'
+    cancelKeyCode: 27,
+    //  line color
+    lineColor: 'red',
+    //  line weight
+    lineWeight: 2,
+    //  line dash
+    lineDashArray: '6, 6',
+    //  line opacity
+    lineOpacity: 1,
+    //  distance formatter
+    // formatDistance: function (val) {
+    //   return Math.round(1000 * val / 1609.344) / 1000 + 'mile';
+    // }
+  }).addTo(map)
