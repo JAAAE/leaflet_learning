db76d2ae072ed8efd95ad3c548a0a7991ab0c9ed
a354794@gmail.com <a354794@gmail.com>
2021-01-02 22:58:15 +0800

    add

--- docs/js/app.js
+++ docs/js/app.js
@@ -143,12 +143,4 @@ axios.get("https://www.geologycloud.tw/api/v1/zh-tw/Fault50?t=.json")
   L.control.locate().addTo(map);
 
 // Fullscreen
-L.control.fullscreen({
-    position: 'topleft', // change the position of the button can be topleft, topright, bottomright or bottomleft, defaut topleft
-    title: 'Show me the fullscreen !', // change the title of the button, default Full Screen
-    titleCancel: 'Exit fullscreen mode', // change the title of the button when fullscreen is on, default Exit Full Screen
-    content: null, // change the content of the button, can be HTML, default null
-    forceSeparateButton: true, // force seperate button to detach from zoom buttons, default false
-    forcePseudoFullscreen: true, // force use of pseudo full screen even if full screen API is available, default false
-    fullscreenElement: false // Dom element to render in full screen, false by default, fallback to map._container
-  }).addTo(map);
+map.addControl(new L.Control.Fullscreen());
